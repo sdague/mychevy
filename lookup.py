3eabb3acbadb8a170f6b403d49293f2211a93874
Sean Dague <sean@dague.net>
2017-12-30 07:16:27 -0500

    refactor for more attributes and testing

--- lookup.py
+++ lookup.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python3
 
-import asyncio
-
 from mychevy.mychevy import MyChevy
 
 import configparser
 
-async def print_data():
+def print_data():
     config = configparser.ConfigParser()
     config.read("config.ini")
 
-    page = MyChevy(config["default"]["user"], config["default"]["passwd"])
+    page = MyChevy(config["default"]["user"], config["default"]["passwd"],
+                   headless=False)
 
-    data = await page.data()
+    data = page.data()
     print(data)
 
 def main():
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(print_data())
-
+    print_data()
 
 main()
--- mychevy/mychevy.py
+++ mychevy/mychevy.py
@@ -23,19 +23,25 @@ TIMEOUT = 120
 class EVCar(object):
 
     def __init__(self, est_range=0, mileage=0, plugged_in=False,
-                 charge_volts=0, charge_percent=0, eta=0):
+                 state="", charge_percent=0, eta=0, charge_mode="",
+                 charging=""):
         super(EVCar, self).__init__()
         self.plugged_in = plugged_in
         self.range = est_range
-        self.volts = charge_volts
         self.percent = charge_percent
         self.mileage = mileage
         self.eta = eta
+        self.state = state
+        self.charge_mode = charge_mode
+        self.charging = charging
 
     def __str__(self):
         return ("<EVCar range=%s miles, bat=%s%%, plugged_in=%s, "
-                "mileage=%s miles>" % (
-                    self.range, self.percent, self.plugged_in, self.mileage))
+                "mileage=%s miles, charging=%s, charge_mode=%s, eta=%s, "
+                "state=%s>" % (
+                    self.range, self.percent, self.plugged_in, self.mileage,
+                    self.charging, self.charge_mode, self.eta, self.state)
+        )
 
 
 class MyChevy(object):
@@ -48,15 +54,51 @@ class MyChevy(object):
         self.passwd = passwd
         self.headless = headless
 
+    def parse_plug(self, driver):
+        status = driver.find_element_by_css_selector(".status-right").text.split("\n")
+        charging = status[0]
 
-    def data(self):
-        # Get the main page
-        chrome_options = Options()
-        if self.headless:
-            chrome_options.add_argument("--headless")
-        driver = webdriver.Chrome(
-            executable_path=os.path.abspath(self.chromedriver),
-            chrome_options=chrome_options)
+        if "Plugged in" in charging:
+            plugged_in = True
+        else:
+            plugged_in = False
+
+        state = charging
+
+        return (plugged_in, state)
+
+    def charging(self, driver):
+        status = driver.find_element_by_css_selector(".status-right").text.split("\n")
+        mode = status[1]
+        return mode
+
+    def range(self, driver):
+        status = driver.find_element_by_css_selector(".status-right").text.split("\n")
+        for i, key in enumerate(status):
+            if "Estimated Electric Range" in key:
+                # strip out units
+                return int(status[i + 1].split(' ')[0])
+
+    def eta(self, driver):
+        status = driver.find_element_by_css_selector(".status-right").text.split("\n")
+        for i, key in enumerate(status):
+            if "Estimated Full Charge" in key:
+                # strip out units
+                return status[i + 1]
+
+    def charge_mode(self, driver):
+        status = driver.find_element_by_css_selector(".status-right").text.split("\n")
+        for i, key in enumerate(status):
+            if "Charge Mode" in key:
+                # strip out units
+                return status[i + 1]
+
+    def mileage(self, driver):
+        mileage = driver.find_element_by_css_selector(
+            ".panel-vehicle-info-table tbody tr:nth-child(1) td:nth-child(2)").text
+        return int(mileage)
+
+    def _load_page(self, driver):
         driver.get(LOGIN_URL)
 
         # Login as user
@@ -71,27 +113,38 @@ class MyChevy(object):
         element_present = EC.presence_of_element_located(
             (By.CLASS_NAME, 'status-box'))
         WebDriverWait(driver, TIMEOUT).until(element_present)
+
+    def data(self):
+        # Get the main page
+        chrome_options = Options()
+        if self.headless:
+            chrome_options.add_argument("--headless")
+        driver = webdriver.Chrome(
+            executable_path=os.path.abspath(self.chromedriver),
+            chrome_options=chrome_options)
+
+        self._load_page(driver)
+
+        # Start pulling out data
         charge = driver.find_element_by_css_selector(".status-box h1").text
-        charge = charge.replace('%', '')
+        charge = int(charge.replace('%', ''))
+        car = EVCar(charge_percent=charge)
 
-        status_r = driver.find_element_by_css_selector(".status-right").text.split("\n")
-        charging = status_r[0]
+        plugged_in, state = self.parse_plug(driver)
+        car.plugged_in = plugged_in
+        car.state = state
 
-        if "Plugged in" in charging:
-            plugged_in = True
+        car.charge_mode = self.charge_mode(driver)
+        car.charging = self.charging(driver)
+        car.eta = self.eta(driver)
+        car.range = self.range(driver)
+        car.charge_mode = self.charge_mode(driver)
+        car.mileage = self.mileage(driver)
+
+        # if we aren't headless, don't close the window at the end
+        if self.headless is True:
+            driver.close()
         else:
-            plugged_in = False
-
-        car = EVCar(plugged_in=plugged_in, charge_percent=charge)
-
-        for i, key in enumerate(status_r):
-            if "Estimated Electric Range" in key:
-                # strip out units
-                car.range = status_r[i + 1].split(' ')[0]
-
-        car.mileage = driver.find_element_by_css_selector(
-            ".panel-vehicle-info-table tbody tr:nth-child(1) td:nth-child(2)").text
-
-        driver.close()
+            self.driver = driver
 
         return car
