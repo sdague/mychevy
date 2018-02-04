da5c17b25f116aed7b15a89fe2830d89f2e5ee5f
Sean Dague <sean@dague.net>
2018-02-04 11:14:07 -0500

    more unit tests

--- tests/test_mychevy.py
+++ tests/test_mychevy.py
@@ -19,6 +19,9 @@ CAR1 = {
     "imageUrl": ""
 }
 
+# DATA packet:
+PKT1 = b'{"messages":[],"serverErrorMsgs":[],"data":{"dataAsOfDate":1516671611000,"batteryLevel":70,"chargeState":"not_charging","plugState":"plugged","rateType":"PEAK","voltage":240,"electricRange":132,"totalRange":132,"chargeMode":"DEPARTURE_BASED","electricMiles":1601,"gasMiles":0,"totalMiles":1601,"percentageOnElectric":1,"fuelEconomy":1000,"electricEconomy":45,"combinedEconomy":11,"fuelUsed":132,"electricityUsed":132,"estimatedGallonsFuelSaved":61.13,"estimatedCO2Avoided":1185.92,"estimatedFullChargeBy":"5:00 a.m."}}'  # noqa
+
 
 class TestMyChevy(unittest.TestCase):
 
@@ -32,3 +35,14 @@ class TestMyChevy(unittest.TestCase):
             car.from_json(
                 '{"messages": [], "serverErrorMsgs": [], '
                 '"data": "SERVER ERROR"}')
+
+    def test_json_parse(self):
+        car = EVCar(CAR1)
+        car.from_json(PKT1)
+
+        assert car.plugged_in is True
+        assert car.batteryLevel == 70
+        assert car.voltage == 240
+        assert car.totalRange == 132
+        assert car.totalMiles == 1601
+        assert car.estimatedFullChargeBy == "5:00 a.m."
