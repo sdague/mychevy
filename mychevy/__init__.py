1a6fdede33b0236d9bf2e390da61662e927c8178
Sean Dague <sean@dague.net>
2019-01-02 13:42:39 -0500

    Bump version: 1.1.0 → 1.2.0

--- mychevy/__init__.py
+++ mychevy/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Sean Dague"""
 __email__ = 'sean@dague.net'
-__version__ = '1.1.0'
+__version__ = '1.2.0'
--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ test_requirements = [
 
 setup(
     name='mychevy',
-    version='1.1.0',
+    version='1.2.0',
     description="Python interface to My Chevy online data",
     long_description=readme + '\n\n' + history,
     author="Sean Dague",
